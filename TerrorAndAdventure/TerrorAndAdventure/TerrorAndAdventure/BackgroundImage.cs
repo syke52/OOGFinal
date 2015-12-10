7c2ef0fd34d4d3905a8b62c421c9cded1af95336
syke52 <sydney.kereliuk@gmail.com>
2015-12-10 16:55:38 -0500

    ,,

--- TerrorAndAdventure/TerrorAndAdventure/TerrorAndAdventure/BackgroundImage.cs
+++ TerrorAndAdventure/TerrorAndAdventure/TerrorAndAdventure/BackgroundImage.cs
@@ -15,27 +15,18 @@ namespace TerrorAndAdventure
     /// <summary>
     /// This is a game component that implements IUpdateable.
     /// </summary>
-    public class Bat : Microsoft.Xna.Framework.DrawableGameComponent
+    public class BackgroundImage : Microsoft.Xna.Framework.DrawableGameComponent
     {
         private SpriteBatch spriteBatch;
         private Texture2D tex;
-        private Vector2 position;
-        private Vector2 speed;
-        private Vector2 stage = new Vector2(Shared.stage.X, Shared.stage.Y);
-
-        public Bat(Game game,
-            SpriteBatch spriteBatch,
-            Texture2D tex,
-            Vector2 position,
-            Vector2 speed)
+        private Rectangle srcRect;
+        public BackgroundImage(Game game, SpriteBatch spriteBatch, Texture2D tex, Rectangle srcRect)
             : base(game)
         {
             // TODO: Construct any child components here
             this.spriteBatch = spriteBatch;
             this.tex = tex;
-            this.position = position;
-            this.speed = speed;
-
+            this.srcRect = srcRect;
         }
 
         /// <summary>
@@ -56,39 +47,15 @@ namespace TerrorAndAdventure
         public override void Update(GameTime gameTime)
         {
             // TODO: Add your update code here
-            KeyboardState ks = Keyboard.GetState();
-            if (ks.IsKeyDown(Keys.Left))
-            {
-                position -= speed;
-                if (position.X < 0)
-                {
-                    position.X = 0;
-                }
-            }
-            if (ks.IsKeyDown(Keys.Right))
-            {
-                position += speed;
-                if (position.X > stage.X - tex.Width)
-                {
-                    position.X = stage.X - tex.Width;
-                }
-            }
 
             base.Update(gameTime);
         }
-
         public override void Draw(GameTime gameTime)
         {
             spriteBatch.Begin();
-            //version 2
-            spriteBatch.Draw(tex, position, Color.White);
+            spriteBatch.Draw(tex, srcRect, Color.White);
             spriteBatch.End();
             base.Draw(gameTime);
-        }
-
-        public Rectangle getBounds()
-        {
-            return new Rectangle((int)position.X, (int)position.Y, tex.Width, tex.Height);
         }
     }
 }
--- TerrorAndAdventure/TerrorAndAdventure/TerrorAndAdventure/BattleScene.cs
+++ TerrorAndAdventure/TerrorAndAdventure/TerrorAndAdventure/BattleScene.cs
@@ -9,28 +9,45 @@ using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 using Microsoft.Xna.Framework.Media;
 
-
 namespace TerrorAndAdventure
 {
     /// <summary>
     /// This is a game component that implements IUpdateable.
     /// </summary>
-    public class BattleScene : GameScene
+    public class BattleScene : Microsoft.Xna.Framework.DrawableGameComponent
     {
+        GraphicsDeviceManager graphics;
         SpriteBatch spriteBatch;
-        Texture2D tex;
-        Bat bat;
-        Vector2 position;
-        Vector2 speed = new Vector2(4, 0);
-        public BattleScene(Game game, SpriteBatch spriteBatch)
+        BackgroundImage background;
+        HeroIdle heroIdle;
+        Texture2D heroTexIdle;
+        Texture2D backgroundImage;
+
+        public BattleScene(Game game, SpriteBatch spriteBatch, GraphicsDeviceManager graphics, int cScene)
             : base(game)
         {
             // TODO: Construct any child components here
             this.spriteBatch = spriteBatch;
-            tex = game.Content.Load<Texture2D>("Images/Bat");
-            position = new Vector2(Shared.stage.X / 2 - tex.Width / 2, Shared.stage.Y - tex.Width);
-            bat = new Bat(game, spriteBatch, tex, position, speed);
-            this.Components.Add(bat);
+            this.graphics = graphics;
+
+            if (cScene <= 4)
+            {
+                backgroundImage = Game.Content.Load<Texture2D>("BattleImages/backgroundForest");
+            }
+            else if (cScene > 4)
+            {
+                backgroundImage = Game.Content.Load<Texture2D>("BattleImages/backgroundCave");
+            }
+            Rectangle r = new Rectangle(0, 0, graphics.PreferredBackBufferWidth, graphics.PreferredBackBufferHeight);
+            background = new BackgroundImage(Game, spriteBatch, backgroundImage, r);
+            this.Game.Components.Add(background);
+
+            //hero idle animation
+            heroTexIdle = Game.Content.Load<Texture2D>("BattleImages/antylamon_sprite_idle2");
+            Vector2 posIdle = new Vector2(graphics.PreferredBackBufferWidth, graphics.PreferredBackBufferHeight/ 2);
+            int delayIdle = 30;
+            heroIdle = new HeroIdle(Game, spriteBatch, heroTexIdle, posIdle, delayIdle);
+            this.Game.Components.Add(heroIdle);
         }
 
         /// <summary>
@@ -40,6 +57,7 @@ namespace TerrorAndAdventure
         public override void Initialize()
         {
             // TODO: Add your initialization code here
+
 
             base.Initialize();
         }
@@ -53,6 +71,14 @@ namespace TerrorAndAdventure
             // TODO: Add your update code here
 
             base.Update(gameTime);
+        }
+
+        public override void Draw(GameTime gameTime)
+        {
+            spriteBatch.Begin();
+            spriteBatch.Draw(backgroundImage, new Rectangle(0, 0, graphics.PreferredBackBufferWidth, graphics.PreferredBackBufferHeight), Color.White);
+            spriteBatch.End();
+            base.Draw(gameTime);
         }
     }
 }
